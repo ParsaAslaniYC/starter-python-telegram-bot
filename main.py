4f8810af22bdfa0dfb0b4b79a0ee897e18d0fa84
itzParsaYC <115403383+ParsaAslaniYC@users.noreply.github.com>
2024-02-12 20:58:48 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -1,45 +1,44 @@
-import os
-from dotenv import load_dotenv
-from fastapi import FastAPI, Header, HTTPException, Depends
-from telegram import Update, Bot
-from pydantic import BaseModel
 
-class TelegramUpdate(BaseModel):
-    update_id: int
-    message: dict
+TOKEN = "6725461751:AAFwo9Q1Nq4lTyB-Mjy6rQQeXzACcHoQjuw"
+import telebot
+from telebot import types
 
-app = FastAPI()
-
-# Load variables from .env file if present
-load_dotenv()
-
-# Read the variable from the environment (or .env file)
-bot_token = os.getenv('BOT_TOKEN')
-secret_token = os.getenv("SECRET_TOKEN")
-# webhook_url = os.getenv('CYCLIC_URL', 'http://localhost:8181') + "/webhook/"
-
-bot = Bot(token=bot_token)
-# bot.set_webhook(url=webhook_url)
-# webhook_info = bot.get_webhook_info()
-# print(webhook_info)
-
-def auth_telegram_token(x_telegram_bot_api_secret_token: str = Header(None)) -> str:
-    # return true # uncomment to disable authentication
-    if x_telegram_bot_api_secret_token != secret_token:
-        raise HTTPException(status_code=403, detail="Not authenticated")
-    return x_telegram_bot_api_secret_token
-
-@app.post("/webhook/")
-async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_telegram_token)):
-    chat_id = update.message["chat"]["id"]
-    text = update.message["text"]
-    # print("Received message:", update.message)
-
-    if text == "/start":
-        with open('hello.gif', 'rb') as photo:
-            await bot.send_photo(chat_id=chat_id, photo=photo)
-        await bot.send_message(chat_id=chat_id, text="Welcome to Cyclic Starter Python Telegram Bot!")
-    else:
-        await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text="Yo!")
-
-    return {"ok": True}
+class Mia:
+    def __init__(self):
+        MiaRoms = telebot.TeleBot(TOKEN)
+        print("Mia is Borned successfully!")
+        self.mr = MiaRoms
+        @self.mr.message_handler(commands=['start', 'hello'])
+        def send_welcome(message):
+            self.mr.reply_to(message, "Howdy, do you need my help to find roms?\ni'm here\nMyCommands:\n/about for about\n/rom for showing roms\n/add_rom for adding rom to database\n/kernel to see kernels\n/add_kernel to add missing kernels to list")
+        
+        @self.mr.message_handler(commands=['rom', 'find'])
+        def send_roms(message):
+            with open("Mia_Roms.db", 'r') as db:
+                rom_list = db.read()
+            self.mr.reply_to(message, rom_list)
+        @self.mr.message_handler(commands=['about'])
+        def send_about(message):
+            self.mr.reply_to(message, "I'm Mia! a bot for helping users to find their rom faster!\nMy father's is @itzParsaYC and @BotFather")
+        @self.mr.message_handler(commands=['add_rom'])
+        def add_rom(message):        
+            self.mr.reply_to(message, "Thanks for adding a new rom to my database!\nPlease send the rom as this format:\n<Rom Name>: <Rom Link>")
+            @self.mr.message_handler(func=lambda message: True)
+            def add(message):
+                with open("Mia_Roms.db", "a") as db:
+                    db.write("\n\n" + message.text)
+                self.mr.reply_to(message, f"Added!")
+        @self.mr.message_handler(commands=['kernel'])
+        def show_kernel(message):
+            with open("Mia_Kernels.db", "r") as db:
+                self.mr.reply_to(message, db.read())
+        @self.mr.message_handler(commands=['add_kernel'])
+        def add_kernel(message):        
+            self.mr.reply_to(message, "Thanks for adding a new kernel to my database!\nPlease send the kernel as this format:\n<Kernel Name>:\n    -KSU: <KSU Kernel Link>\n    -nonKSU: <nonKSU Kernel Link>")
+            @self.mr.message_handler(func=lambda message: True)
+            def add(message):
+                with open("Mia_Kernels.db", "a") as db:
+                    db.write("\n\n" + message.text)
+                self.mr.reply_to(message, f"Added!")
+        self.mr.infinity_polling()
+Mia()
